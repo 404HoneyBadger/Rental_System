0c3f69aada55267f0ecbda2ad6ce2d82049a8ff7
404HoneyBadger <94186840+404HoneyBadger@users.noreply.github.com>
2022-04-29 08:22:10 -0400

    Update rental-system.sql

--- rental-system.sql
+++ rental-system.sql
@@ -1,89 +1,84 @@
-DROP DATABASE IF EXISTS `rental_db`;
-CREATE DATABASE `rental_db`;
-USE `rental_db`;
- 
--- Create `vehicles` table
-DROP TABLE IF EXISTS `vehicles`;
-CREATE TABLE `vehicles` (
-   `veh_reg_no`  VARCHAR(8)    NOT NULL,
-   `category`    ENUM('car', 'truck')  NOT NULL DEFAULT 'car',  
-                 -- Enumeration of one of the items in the list
-   `brand`       VARCHAR(30)   NOT NULL DEFAULT '',
-   `desc`        VARCHAR(256)  NOT NULL DEFAULT '',
-                 -- desc is a keyword (for descending) and must be back-quoted
-   `photo`       BLOB          NULL,   -- binary large object of up to 64KB
-                 -- to be implemented later
-   `daily_rate`  DECIMAL(6,2)  NOT NULL DEFAULT 9999.99,
-                 -- set default to max value
-   PRIMARY KEY (`veh_reg_no`),
-   INDEX (`category`)  -- Build index on this column for fast search
-) ENGINE=InnoDB;
-   -- MySQL provides a few ENGINEs.
-   -- The InnoDB Engine supports foreign keys and transactions
-DESC `vehicles`;
-SHOW CREATE TABLE `vehicles`;
-SHOW INDEX FROM `vehicles`;
- 
--- Create `customers` table
-DROP TABLE IF EXISTS `customers`;
-CREATE TABLE `customers` (
-   `customer_id`  INT UNSIGNED  NOT NULL AUTO_INCREMENT,
-                  -- Always use INT for AUTO_INCREMENT column to avoid run-over
-   `name`         VARCHAR(30)   NOT NULL DEFAULT '',
-   `address`      VARCHAR(80)   NOT NULL DEFAULT '',
-   `phone`        VARCHAR(15)   NOT NULL DEFAULT '',
-   `discount`     DOUBLE        NOT NULL DEFAULT 0.0,
-   PRIMARY KEY (`customer_id`),
-   UNIQUE INDEX (`phone`),  -- Build index on this unique-value column
-   INDEX (`name`)           -- Build index on this column
-) ENGINE=InnoDB;
-DESC `customers`;
-SHOW CREATE TABLE `customers`;
-SHOW INDEX FROM `customers`;
- 
--- Create `rental_records` table
-DROP TABLE IF EXISTS `rental_records`;
-CREATE TABLE `rental_records` (
-   `rental_id`    INT UNSIGNED  NOT NULL AUTO_INCREMENT,
-   `veh_reg_no`   VARCHAR(8)    NOT NULL, 
-   `customer_id`  INT UNSIGNED  NOT NULL,
-   `start_date`   DATE          NOT NULL DEFAULT('0000-00-00'),
-   `end_date`     DATE          NOT NULL DEFAULT('0000-00-00'),
-   `lastUpdated`  TIMESTAMP     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-      -- Keep the created and last updated timestamp for auditing and security
-   PRIMARY KEY (`rental_id`),
-   FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`)
-      ON DELETE RESTRICT ON UPDATE CASCADE,
-      -- Disallow deletion of parent record if there are matching records here
-      -- If parent record (customer_id) changes, update the matching records here
-   FOREIGN KEY (`veh_reg_no`) REFERENCES `vehicles` (`veh_reg_no`)
-      ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE=InnoDB;
-DESC `rental_records`;
-SHOW CREATE TABLE `rental_records`;
-SHOW INDEX FROM `rental_records`;
+-- 1. Customer 'Angel' has rented 'SBA1111A' from today for 10 days
+insert into rental_records(veh_reg_no, customer_id, start_date, end_date)
+Values ('SBA1111A',
+(select customer_id
+from customers
+where name='Angel'),
+(select curdate()),
+(date_add(curdate(), interval 10 day)));
 
--- Inserting test records
-INSERT INTO `vehicles` VALUES
-   ('SBA1111A', 'car', 'NISSAN SUNNY 1.6L', '4 Door Saloon, Automatic', NULL, 99.99),
-   ('SBB2222B', 'car', 'TOYOTA ALTIS 1.6L', '4 Door Saloon, Automatic', NULL, 99.99),
-   ('SBC3333C', 'car', 'HONDA CIVIC 1.8L',  '4 Door Saloon, Automatic', NULL, 119.99),
-   ('GA5555E', 'truck', 'NISSAN CABSTAR 3.0L',  'Lorry, Manual ', NULL, 89.99),
-   ('GA6666F', 'truck', 'OPEL COMBO 1.6L',  'Van, Manual', NULL, 69.99);
-   -- No photo yet, set to NULL
-SELECT * FROM `vehicles`;
- 
-INSERT INTO `customers` VALUES
-   (1001, 'Angel', '8 Happy Ave', '88888888', 0.1),
-   (NULL, 'Mohammed Ali', '1 Kg Java', '99999999', 0.15),
-   (NULL, 'Kumar', '5 Serangoon Road', '55555555', 0),
-   (NULL, 'Kevin Jones', '2 Sunset boulevard', '22222222', 0.2);
-SELECT * FROM `customers`;
- 
-INSERT INTO `rental_records` VALUES
-  (NULL, 'SBA1111A', 1001, '2012-01-01', '2012-01-21', NULL),
-  (NULL, 'SBA1111A', 1001, '2012-02-01', '2012-02-05', NULL),
-  (NULL, 'GA5555E',  1003, '2012-01-05', '2012-01-31', NULL),
-  (NULL, 'GA6666F',  1004, '2012-01-20', '2012-02-20', NULL);
-SELECT * FROM `rental_records`;
+-- 2. Customer 'Kumar' has rented 'GA5555E' from tomorrow for 3 months.
+insert into rental_records(veh_reg_no, customer_id, start_date, end_date)
+Values ('GA5555E',
+(select customer_id
+from customers
+where name='Kumar'),
+(select curdate()+1),
+(date_add(curdate()+1, interval 3 MONTH)));
+
+-- 3. List all rental records (start date, end date) with vehicle's registration number, brand, and customer name, sorted by vehicle's categories followed by start date
+drop view all_rental_records;
+
+create view all_rental_records
+as select start_date, end_date, vehicles.veh_reg_no, brand, name
+from vehicles, rental_records, customers
+where vehicles.veh_reg_no=rental_records.veh_reg_no
+and rental_records.customer_id=customers.customer_id
+order by vehicles.category, start_date;
+
+select * from all_rental_records;
+
+-- 4. List all the expired rental records (end_date before CURDATE()).
+select *
+from rental_records
+where end_date<curdate();
+
+-- 5. List the vehicles rented out on '2012-01-10', in columns of vehicle registration no, customer name, start date and end date
+drop view not_available;
+
+create view not_available
+as select rental_records.veh_reg_no, name, start_date, end_date
+from rental_records, customers
+where rental_records.customer_id=customers.customer_id
+and start_date>'2012-01-10'<end_date;
+
+select * from not_available;
+
+-- 6. List all vehicles rented out today, in columns registration number, customer name, start date, end date.
+select veh_reg_no, name, start_date, end_date
+from rental_records, customers
+where rental_records.customer_id=customers.customer_id
+and start_date=curdate();
+
+-- 7. Similarly, list the vehicles rented out (not available for rental) for the period from '2012-01-03' to '2012-01-18'
+select veh_reg_no, name, start_date, end_date
+from rental_records, customers
+where rental_records.customer_id=customers.customer_id
+and start_date between '2012-01-03' and '2012-01-18';
+
+-- 8. List the vehicles (registration number, brand and description) available for rental (not rented out) on '2012-01-10'
+drop view date_available;
+
+select rental_records.veh_reg_no, brand, vehicles.desc
+from vehicles
+left join rental_records
+on vehicles.veh_reg_no=rental_records.veh_reg_no
+and not (start_date<'2012-01-10' and end_date>'2012-01-10');
+
+-- 9. Similarly, list the vehicles available for rental for the period from '2012-01-03' to '2012-01-18'.
+select rental_records.veh_reg_no, brand, vehicles.desc
+from vehicles
+left join rental_records
+on vehicles.veh_reg_no=rental_records.veh_reg_no
+and not ((start_date between '2012-01-03' AND '2012-01-18')
+ OR (end_date between '2012-01-03' AND end_date > '2012-01-18')
+ OR (start_date < '2012-01-03' AND end_date > '2012-01-18'));
+
+-- 10. Similarly, list the vehicles available for rental from today for 10 days
+SELECT r.veh_reg_no, v.brand, v.desc
+ FROM vehicles AS v LEFT JOIN rental_records AS r 
+ ON  v.veh_reg_no = r.veh_reg_no
+ AND NOT ((start_date between curdate() AND (date_add(curdate(), interval 10 day)))
+ OR (end_date between curdate() AND end_date (date_add(curdate(), interval 10 day)))
+ OR (start_date < curdate() AND end_date > (date_add(curdate(), interval 10 day))) );
 
